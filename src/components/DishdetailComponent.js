01a751d0c1c906c244d950749a71c8c80d184c7a
Reetik07 <guptareetik07@gmail.com>
2023-02-11 21:14:10 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,7 +28,7 @@ class Dishdetail extends Component {
                 return(
                     <li key={comment.id}>
                         <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {comment.date}</p>
+                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                     </li>
                 );
             });
@@ -50,15 +50,17 @@ class Dishdetail extends Component {
     }
 
     render() {
-        if (this.props.selectedDish != null) {
+        if (this.props.dish != null) {
             return(
+                <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.selectedDish)}
+                        {this.renderDish(this.props.dish)}
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.selectedDish.comments)}
+                        {this.renderComments(this.props.dish.comments)}
                     </div>
+                </div>
                 </div>
             );
         }
